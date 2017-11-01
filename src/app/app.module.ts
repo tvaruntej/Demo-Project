0863f853b0d765baa5f91e2406ae179ceac601fe
tvaruntej <tvaruntej@yahoo.com>
2017-11-01 13:22:24 -0500

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatToolbarModule} from '@angular/material';
+import {MatCardModule} from '@angular/material';
+import {MatSidenavModule} from '@angular/material';
+import {MatMenuModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material';
+import {MatIconModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { TestComponentComponent } from './test-component/test-component.component';
+import {MatSelectModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material';
+import {MatFormFieldControl} from '@angular/material';
+import {MAT_PLACEHOLDER_GLOBAL_OPTIONS} from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_PLACEHOLDER_GLOBAL_OPTIONS, useValue: {float: 'always'}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
