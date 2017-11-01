4ebd4b5f91f0be885a3897b1a8330a1bf2a4b1ae
tvaruntej <tvaruntej@yahoo.com>
2017-11-01 13:20:34 -0500

    Add files via upload

---
+++ src/app/test-component/test-component.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-component',
+  templateUrl: './test-component.component.html',
+  styleUrls: ['./test-component.component.css'],
+})
+export class TestComponentComponent implements OnInit {
+
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
